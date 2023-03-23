3344dd8b1d0a3189a64bf4841ef2753410fe7d0b
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2023-03-23 17:32:42 +0100

    ALG-00 initial commit

---
+++ src/main/java/cloud/cholewa/simplealgorithms/SimpleAlgorithmsApplication.java
@@ -0,0 +1,13 @@
+package cloud.cholewa.simplealgorithms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleAlgorithmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleAlgorithmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cloud/cholewa/simplealgorithms/SimpleAlgorithmsApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.cholewa.simplealgorithms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleAlgorithmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
